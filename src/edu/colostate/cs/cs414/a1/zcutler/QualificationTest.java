230ba3d97f38e03ab3d69a4d7f6c005acd47feb0
zcutler <cutlerz6789@gmail.com>
2017-08-28 18:45:09 -0500

    porting back and forth from intellij to eclipse test

--- src/edu/colostate/cs/cs414/a1/zcutler/QualificationTest.java
+++ src/edu/colostate/cs/cs414/a1/zcutler/QualificationTest.java
@@ -1,10 +1,12 @@
 package edu.colostate.cs.cs414.a1.zcutler;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 import java.util.HashSet;
 
-import org.junit.Test;
+import static org.testng.AssertJUnit.*;
 
 public class QualificationTest {
 
@@ -13,50 +15,50 @@ public class QualificationTest {
 		Qualification x = new Qualification("x");
 		assertSame(x.toString(), "x");
 	}
-	
+
 	@Test
 	public void testToStringFail() {
 		Qualification x = new Qualification("x");
 		assertNotSame(x.toString(), "xyz");
 	}
-	
+
 	private Worker getWorkerWithQualifications() {
 		Qualification x = new Qualification("x");
 		Qualification y = new Qualification("y");
 		Qualification z = new Qualification("z");
-		
-		HashSet<Qualification> qualifications = new HashSet<Qualification>();
+
+		HashSet<Qualification> qualifications = new HashSet<>();
 		qualifications.add(x);
 		qualifications.add(y);
 		qualifications.add(z);
-		
+
 		Worker lyle = new Worker("chippah", qualifications);
-		
+
 		return lyle;
 	}
-	
+
 	private Qualification getQualificationWithMultipleWorkers() {
 		Qualification x = new Qualification("x");
-				
-		HashSet<Qualification> qualifications = new HashSet<Qualification>();
+
+		HashSet<Qualification> qualifications = new HashSet<>();
 		qualifications.add(x);
-		
+
 		@SuppressWarnings("unused")
 		Worker lyle = new Worker("chippah", qualifications);
 		@SuppressWarnings("unused")
 		Worker paul = new Worker("uncle paul", qualifications);
 		@SuppressWarnings("unused")
 		Worker Edger = new Worker("edger", qualifications);
-		
+
 		return x;
 	}
-	
+
 	@Test
 	public void testAddToWorker() {
 		Worker lyle = this.getWorkerWithQualifications();
 		assertEquals(lyle.getQualifications().size(), 3);
 	}
-	
+
 	@Test
 	public void testAddExistingQualificationToWorker() {
 		Worker lyle = this.getWorkerWithQualifications();
@@ -64,14 +66,21 @@ public class QualificationTest {
 		lyle.addQualification(z);
 		assertEquals(lyle.getQualifications().size(), 3);
 	}
-	
+
 	@Test
 	public void testQualificationsWithMultipleWorkers() {
 		Qualification x = getQualificationWithMultipleWorkers();
 		assertEquals(x.getWorkers().size(), 3);
 	}
-	
-	
-	
+
+	@Test
+	public void testQualificationsWithDuplicateWorkers() {
+		Qualification x = getQualificationWithMultipleWorkers();
+		HashSet<Qualification> qualifications = new HashSet<>();
+		qualifications.add(x);
+		Worker lyle = new Worker("chippah", qualifications);
+		x.addWorker(lyle);
+		assertEquals(x.getWorkers().size(), 3);
+	}
 
 }
